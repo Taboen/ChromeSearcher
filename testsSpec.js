cc650595f9500a14f9b20f615b271f7ae6b6fdf2
pawelmhm <pawelmhm@gmail.com>
2014-01-07 13:28:51 +0000

    added tests with Jasmine, test output redirected to console

---
+++ testsSpec.js
@@ -0,0 +1,9 @@
+describe("A suite", function () {
+    it("contains an expectation", function () {
+        expect(true).toBe(true);
+    });
+    it("contains something else",function () {
+        expect(false).toBe(true);
+    });
+});
+
